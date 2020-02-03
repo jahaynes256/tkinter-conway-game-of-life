d8275fd91658fb5d8bacc1c1773b149f735e70bb
James Haynes <jahaynes256@gmail.com>
2020-02-02 20:27:40 -0500

    Clean Up - Cells still act unusual on edges.

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,80 +1,94 @@
-import pprint
-import random
-import time
-from tkinter import Canvas, Tk
-import multiprocessing.dummy as mp 
+import pprint, random, time
+from tkinter import Canvas, Tk, Button, Frame
 
-rows = 50
-cols = 50
-cellSize = 10
+# SETTINGS
+ROWS = 35
+COLS = 35
+CELL_SIZE = 15
 
-def randMatrix():
-    return [[random.random() < 0.20 for j in range(cols)] for i in range(rows)]
+# 0 to 1, Chance for Cell to be Alive on Reset
+INIT_CHANCE = 0.08 
 
+# Using torus geometry, wraps around to other side
 
-def getNeighbors(cellAlive,x,y): #Possibly Logically Incorrect
+def getNeighbors(cellAlive,x,y):
+
+    """
+    If there is living cell adjacent to cell, count it and return it.
+    """
+
     count = 0
     for j in range((y-1),(y+2)):
         for i in range((x-1),(x+2)):
             if not ((i == x) and (j == y)):
-                if cellAlive[i%cols][j%rows]:
+                if cellAlive[i%COLS][j%ROWS]:
                     count += 1
     return count
 
 
-
 def checkForStateChange(cellAlive,checkTime=False):
-    if checkTime:
-        start = time.time_ns()
-        for j in range(rows):
-            for i in range(cols):
-                n = getNeighbors(cellAlive,i,j)
-                if cellAlive[i][j]:
-                    if not((n==2) or (n==3)):
-                        cellAlive[i][j] = False
-                else:
-                    if n == 3:
-                        cellAlive[i][j] = True
-        print(f'checkForState Executions/sec: {1/((time.time_ns() - start)/1000000000.0):.2f}')
-    else:
-        for j in range(rows):
-            for i in range(cols):
-                n = getNeighbors(cellAlive,i,j)
-                if cellAlive[i][j]:
-                    if not(2 <= n <= 3):
-                        cellAlive[i][j] = False
-                else:
-                    if n == 3:
-                        cellAlive[i][j] = True
+
+    """
+    After receiving the number of neighbors from getNeighbors(...) decides
+    if that cells lives or dies. When the function is called, all cells
+    are checked.
+    """
+
+    start = time.time_ns()
+    for j in range(ROWS):
+        for i in range(COLS):
+            n = getNeighbors(cellAlive,i,j)
+            if cellAlive[i][j]:
+                if not((n==2) or (n==3)):
+                    cellAlive[i][j] = False
+            else:
+                if n == 3:
+                    cellAlive[i][j] = True
+
         
         
 
 class drawingTest(object):
+    
+    """
+    tkinter GUI Elements
+    """
 
     def __init__(self):
-        self.windowWidth = cols * cellSize
-        self.windowHeight = rows * cellSize
+        self.windowWidth = COLS * CELL_SIZE
+        self.windowHeight = ROWS * CELL_SIZE
         self.root = Tk()
 
         self.dispCanvas = Canvas(self.root, width=self.windowWidth, height=self.windowHeight)
         
         self.dispCanvas.pack()
 
-        self.myCircle = [[(self.dispCanvas.create_rectangle(cellSize*i,cellSize*j,cellSize*(i+1),cellSize*(j+1), fill='#222222'))for j in range(cols)] for i in range(rows)]
+        self.myCircle = [[(self.dispCanvas.create_rectangle(CELL_SIZE*i,CELL_SIZE*j,CELL_SIZE*(i+1),CELL_SIZE*(j+1), fill='#222222'))for j in range(COLS)] for i in range(ROWS)]
         self.dispCanvas.pack()
-        self.cellAlive = randMatrix()
-
+        self.toolbar = Frame(self.root)
+        
+        self.resetButton = Button(self.toolbar, text="Reset", command=self.resetCells)
+        self.resetButton.pack()
+        
+        self.toolbar.pack()
+        self.cellAlive = self.randMatrix()
         self.root.after(0, self.animation(checkTime=True))
-
         self.root.mainloop()
+    
+    def randMatrix(self):
+        return [[random.random() < INIT_CHANCE for j in range(COLS)] for i in range(ROWS)]
+
+
+    def resetCells(self):
+        self.cellAlive = self.randMatrix()
 
     def animation(self, checkTime=False):
         if checkTime:
             while True:
                 start = time.time_ns()
                 #DISPLAY FRAME
-                for y in range(rows):
-                    for x in range(cols):
+                for y in range(ROWS):
+                    for x in range(COLS):
                         if self.cellAlive[x][y]:
                             self.dispCanvas.itemconfig(self.myCircle[x][y], fill='green')
                         else:
@@ -86,8 +100,8 @@ class drawingTest(object):
         else:
             while True:
                 #DISPLAY FRAME
-                for y in range(rows):
-                    for x in range(cols):
+                for y in range(ROWS):
+                    for x in range(COLS):
                         if self.cellAlive[x][y]:
                             self.dispCanvas.itemconfig(self.myCircle[x][y], fill='green')
                         else:
@@ -104,16 +118,16 @@ class drawingTest(object):
             for i in range((x-1),(x+2)):
                 print(f'({i},{j})')
                 if not ((i == x) and (j == y)):
-                    self.dispCanvas.itemconfig(self.myCircle[i%cols][j%rows], fill='purple')
+                    self.dispCanvas.itemconfig(self.myCircle[i%COLS][j%ROWS], fill='purple')
                 else:
-                    self.dispCanvas.itemconfig(self.myCircle[i%cols][j%rows], fill='blue')
+                    self.dispCanvas.itemconfig(self.myCircle[i%COLS][j%ROWS], fill='blue')
 
 
     def highlightCorners(self):
         self.dispCanvas.itemconfig(self.myCircle[0][0], fill='red')
-        self.dispCanvas.itemconfig(self.myCircle[cols-1][0], fill='red')
-        self.dispCanvas.itemconfig(self.myCircle[0][rows-1], fill='red')
-        self.dispCanvas.itemconfig(self.myCircle[cols-1][rows-1], fill='red')
+        self.dispCanvas.itemconfig(self.myCircle[COLS-1][0], fill='red')
+        self.dispCanvas.itemconfig(self.myCircle[0][ROWS-1], fill='red')
+        self.dispCanvas.itemconfig(self.myCircle[COLS-1][ROWS-1], fill='red')
     #------END DEBUG METHODS------
 
 
